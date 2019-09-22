cd8c391fbdf59c9ec129a1bc668110f1614822ed
Dima <belokolodskikh@gmail.com>
2019-09-22 20:57:02 +0200

    adding new classes with error

--- react-quiz/src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
+++ react-quiz/src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.js
@@ -2,9 +2,17 @@ import React from 'react';
 import classes from './AnswerItem.scss';
 
 const AnswerItem = props => {
-    console.log(props);
+    
+    const cls = [AnswerItem]
+
+    if (props.state) {
+        cls.push([props.state])
+    }
+
     return (
-        <li className ='AnswerItem'
+        <li className= 'AnswerItem' //{cls.join(' ')}
+            // className= 'AnswerItem.success'
+            // className= 'AnswerItem.error'
             onClick={() => props.onAnswerClick(props.answer.id)}
         >
             {props.answer.text}
