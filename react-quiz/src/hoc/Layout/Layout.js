960f0dfe5df85578ee32539d8a24e08aa71377fb
Dima <belokolodskikh@gmail.com>
2019-11-05 20:16:53 +0100

    navbar without classes

--- react-quiz/src/hoc/Layout/Layout.js
+++ react-quiz/src/hoc/Layout/Layout.js
@@ -1,17 +1,36 @@
 // 'use strict';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.scss';
+import MenuToggle from './../../components/Navigation/MenuToggle/MenuToggle';
+
 
 class Layout extends Component {
-   render() {
-       return(
-           <div className='Layout'>
-               <main>
-                   {this.props.children}
-               </main>
-           </div>
-       )
-   }
+
+    state = {
+        menu: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
+    render() {
+        return (
+            <div className='Layout'>
+
+                <MenuToggle
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
+
+                <main>
+                    {this.props.children}
+                </main>
+            </div>
+        )
+    }
 }
 
 export default Layout
